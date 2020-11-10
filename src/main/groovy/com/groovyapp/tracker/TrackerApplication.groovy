665fce4900f15753f11424abb33fcd26efce6b4e
ponchione <mitchell.ponchione@protonmail.com>
2020-11-10 16:06:49 -0500

    Initial groovy api commit

---
+++ src/main/groovy/com/groovyapp/tracker/TrackerApplication.groovy
@@ -0,0 +1,13 @@
+package com.groovyapp.tracker
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TrackerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TrackerApplication, args)
+	}
+
+}
